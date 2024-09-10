f46be9bfe411489c0a8d59ff75d40f98cfc39374
Alissonps15 <alissonportodossantos@gmail.com>
2024-09-10 19:27:47 -0300

    commit

--- src/main/java/br/edu/famper/cadastroprojeto/model/Projeto.java
+++ src/main/java/br/edu/famper/cadastroprojeto/model/Projeto.java
@@ -3,24 +3,35 @@ package br.edu.famper.cadastroprojeto.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+
+
 import java.time.LocalDate;
 
 
 @Entity
 @Table(name = "tbl_projeto")
 @Data
+
 public class Projeto {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "projeto_id")
     private Long codigo;
 
+
+    @Column(name= "nome", length = 150, nullable = false)
+    private String nome;
+
+
     @Column(name= "descricao", length = 150)
     private String descricao;
 
-    @Column(name = "dataInicio")
+
+    @Column(name = "dataInicio", nullable = false)
     private LocalDate dataInicio;
 
     @Column(name = "dataFim")
     private LocalDate dataFim;
+
+
 }
